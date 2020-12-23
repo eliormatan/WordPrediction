ab58de7f345ffd15fd7447b63ca8489cd9a3cac5
awadibra <awadibra86@gmail.com>
2020-12-23 17:29:01 +0200

    divide updated.

--- src/main/java/Divide.java
+++ src/main/java/Divide.java
@@ -1,62 +1,41 @@
 import org.apache.hadoop.conf.Configuration;
 import org.apache.hadoop.fs.Path;
-import org.apache.hadoop.io.ArrayWritable;
 import org.apache.hadoop.io.IntWritable;
 import org.apache.hadoop.io.LongWritable;
 import org.apache.hadoop.io.Text;
 import org.apache.hadoop.mapreduce.*;
 import org.apache.hadoop.mapreduce.lib.input.FileInputFormat;
 import org.apache.hadoop.mapreduce.lib.input.SequenceFileInputFormat;
-import org.apache.hadoop.mapreduce.lib.input.TextInputFormat;
 import org.apache.hadoop.mapreduce.lib.output.FileOutputFormat;
 import org.apache.hadoop.mapreduce.lib.output.TextOutputFormat;
 
 import java.io.IOException;
-import java.util.Arrays;
-import java.util.regex.Matcher;
-import java.util.regex.Pattern;
 
 
 public class Divide {
 
-    public static class MapperClass extends Mapper<LongWritable,Text,Text,ArrayWritable> {
-        private static Pattern HEBREW_PATTERN = Pattern.compile("^[א-ת]+.*$|^\\d*\\s[א-ת]+.*$");
+    public static class MapperClass extends Mapper<LongWritable,Text,Text,Text> {
         @Override
         public void setup(Context context)  throws IOException, InterruptedException {
         }
 
         @Override
         public void map(LongWritable rowNumber, Text tabSeparatedData, Context context) throws IOException,  InterruptedException {
-            LongWritable[] occurrences=new LongWritable[2];
+            Text occurences = new Text();
             String[] splitArray = tabSeparatedData.toString().split("\t");
-            String data = tabSeparatedData.toString();
-            System.out.println("data to string :");
-            System.out.println(data);
-//            Matcher m = HEBREW_PATTERN.matcher(splitArray[0]);
-//            if(m.matches()){
-//                System.out.println("matches :");
-//                System.out.println(m.group());
-//            }
-//
-//            String regexHebrewPattern = "([\\p{InHebrew}]+)";
-//            Pattern patternHebrew = Pattern.compile(regexHebrewPattern, Pattern.UNICODE_CASE);
-//            Matcher hebrewMatcher = patternHebrew.matcher(splitArray[0]);
-//            if(hebrewMatcher.matches()){
-//                System.out.println("second matcher :");
-//                System.out.println(hebrewMatcher.group());
-//            }
+//            String data = tabSeparatedData.toString();
+//            System.out.println("data to string :");
+//            System.out.println(data);
 
-//            //todo:use regex to get rid of spaces...
-//            Text currGram=new Text(splitArray[0]);
-//            long currOc=Long.parseLong(splitArray[2]);
-//            if(rowNumber.get()%2==1){
-//                occurrences[0].set(0);
-//                occurrences[1].set(currOc);
-//            }else {
-//                occurrences[0].set(currOc);
-//                occurrences[1].set(0);
-//            }
-//            context.write(currGram,new ArrayWritable(LongWritable.class,occurrences));
+            //todo:use regex to get rid of spaces...
+            Text currGram=new Text(splitArray[0]);
+            if(rowNumber.get()%2==1){
+                occurences.set(String.format("%s\t%s","0",splitArray[2]));
+
+            }else {
+                occurences.set(String.format("%s\t%s",splitArray[2],"0"));
+            }
+            context.write(currGram,occurences);
         }
 
         @Override
@@ -65,8 +44,33 @@ public class Divide {
 
     }
 
+    public static class CombinerClass extends Reducer<Text,Text,Text,Text> {
 
-    public static class ReducerClass extends Reducer<Text,ArrayWritable,Text,ArrayWritable> {
+        @Override
+        public void setup(Context context)  throws IOException, InterruptedException {
+        }
+
+        @Override
+        public void reduce(Text nGram, Iterable<Text> occurences, Context context) throws IOException,  InterruptedException {
+            long even=0L;
+            long odd=0L;
+            for(Text occ:occurences){
+                String[] splitOcc = occ.toString().split("\t");
+                even+=Long.parseLong(splitOcc[0]);
+                odd+=Long.parseLong(splitOcc[1]);
+            }
+            Text occSum = new Text();
+            occSum.set(String.format("%d\t%d",even, odd));
+
+            context.write(nGram,occSum);
+        }
+
+        @Override
+        public void cleanup(Context context)  throws IOException, InterruptedException {
+        }
+    }
+
+    public static class ReducerClass extends Reducer<Text,Text,Text,Text> {
         private Counter N;
         @Override
         public void setup(Context context)  throws IOException, InterruptedException {
@@ -74,19 +78,18 @@ public class Divide {
         }
 
         @Override
-        public void reduce(Text threeGram, Iterable<ArrayWritable> occurencesArrays, Context context) throws IOException,  InterruptedException {
-            long even=0;
-            long odd=0;
-            LongWritable[] occurrences=new LongWritable[2];
-            for(ArrayWritable arr:occurencesArrays){
-                LongWritable[] currOc=(LongWritable[]) arr.get();
-                even+=currOc[0].get();
-                odd+=currOc[1].get();
+        public void reduce(Text nGram, Iterable<Text> occurences, Context context) throws IOException,  InterruptedException {
+            long even=0L;
+            long odd=0L;
+            for(Text occ:occurences){
+                String[] splitOcc = occ.toString().split("\t");
+                even+=Long.parseLong(splitOcc[0]);
+                odd+=Long.parseLong(splitOcc[1]);
             }
-            occurrences[0].set(even);
-            occurrences[1].set(odd);
+            Text occSum = new Text();
+            occSum.set(String.format("%d\t%d",even, odd));
 
-            context.write(threeGram,new ArrayWritable(LongWritable.class,occurrences));
+            context.write(nGram,occSum);
 
             N.increment(even+odd);
         }
@@ -115,13 +118,14 @@ public class Divide {
         job.setMapperClass(MapperClass.class);
         job.setPartitionerClass(PartitionerClass.class);
         job.setReducerClass(ReducerClass.class);
+        job.setCombinerClass(CombinerClass.class);
         job.setMapOutputKeyClass(Text.class);
-        job.setMapOutputValueClass(ArrayWritable.class);
+        job.setMapOutputValueClass(Text.class);
         job.setOutputKeyClass(Text.class);
-        job.setOutputValueClass(ArrayWritable.class);
+        job.setOutputValueClass(Text.class);
         job.setInputFormatClass(SequenceFileInputFormat.class);
-//        job.setOutputFormatClass(TextOutputFormat.class);
-        SequenceFileInputFormat.addInputPath(job, new Path(args[0]));
+        job.setOutputFormatClass(TextOutputFormat.class);
+        FileInputFormat.addInputPath(job, new Path(args[0]));
         FileOutputFormat.setOutputPath(job, new Path(args[1]));
         System.exit(job.waitForCompletion(true) ? 0 : 1);
     }
